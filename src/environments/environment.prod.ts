ec3469ec8d921a5c594e50576b6b946324146308
Philip Andes <philip__andes@hotmail.nl>
2019-06-07 17:45:06 +0200

    AngularFire2 Setup & Client Service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCaANIAAG1wEInx6HTNsFAZ4CJbzx4pdpM",
+    authDomain: "clientpanelprod-cf2ea.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-cf2ea.firebaseio.com",
+    projectId: "clientpanelprod-cf2ea",
+    storageBucket: "clientpanelprod-cf2ea.appspot.com",
+    messagingSenderId: "261443344598",
+    appId: "1:261443344598:web:eea1ac048041eab6"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCaANIAAG1wEInx6HTNsFAZ4CJbzx4pdpM",
+    authDomain: "clientpanelprod-cf2ea.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-cf2ea.firebaseio.com",
+    projectId: "clientpanelprod-cf2ea",
+    storageBucket: "clientpanelprod-cf2ea.appspot.com",
+    messagingSenderId: "261443344598",
+    appId: "1:261443344598:web:eea1ac048041eab6"
+  }
 };
 
 /*
