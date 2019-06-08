e95a9da87129621909a39967ad5955a72a6d9141
Philip Andes <philip__andes@hotmail.nl>
2019-06-08 11:54:11 +0200

    Client Balance Update

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -24,7 +24,7 @@ export class ClientsComponent implements OnInit {
   getTotalOwed() {
     // Reduce works similuar to forEach
     this.totalOwed = this.clients.reduce((total, client) => {
-      return total + client.balance;
+      return total + parseFloat(client.balance.toString());
     }, 0);
   }
 
